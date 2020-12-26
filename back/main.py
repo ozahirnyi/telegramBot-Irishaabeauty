1f5f31e92c4e0f18436fee4ef083c2ce4a50ff9e
ozahirnyi <ozahirny@gmail.com>
2020-12-26 15:10:53 +0200

    kostyak gotov k napolneniu kontentom)

--- back/main.py
+++ back/main.py
@@ -3,12 +3,9 @@ import telebot as tb
 bot = tb.TeleBot('1481133710:AAHc6yyPa3bQJcV86C2HQ9xBmpxNPh-h1hk')
 
 # Main keyboard
-keyboard1 = tb.types.ReplyKeyboardMarkup(True)
-keyboard1.row('Price', 'Contacts', 'Help')
-help_str = "*Price:* Типы макиажа.\n          Цены.\n\n" \
-           "*Contacts:* телега - ...\n" \
-           "                инста - ...\n" \
-           "                номер - ...\n"
+main_keyboard = tb.types.ReplyKeyboardMarkup(True)
+main_keyboard.row('Price', 'Contacts', 'Help')
+
 # Start keyboard
 s_key_b1 = tb.types.InlineKeyboardButton(text="Заказать", callback_data="Zakazat")
 s_key_b2 = tb.types.InlineKeyboardButton(text="Подобрать", callback_data="Vibrat")
@@ -20,7 +17,7 @@ start_keyboard.add(s_key_b2, s_key_b1)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет! Это телеграм-бот @irishaa_antonova, чем могу помочь?',
-                     reply_markup=start_keyboard)
+                     reply_markup=main_keyboard)
 
 
 # Main button pressed
@@ -37,6 +34,18 @@ def parse_input_message(message):
         bot.send_message(message.chat.id, "instagram: @irisha_abeauty"
                                           "\n\ntelegram: @irishaa_antonova"
                                           "\n\nphone: +380952180492")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def help_handler(call):
+    if call.message:
+        if call.data == "Zakazat":
+            for i in range(0, 5):
+                img = open("resources/test_screen.png", 'rb')
+                bot.send_message(call.message.chat.id, "Rabota #" + str(i))
+                bot.send_photo(call.message.chat.id, img)
+        elif call.data == "Vibrat":
+            bot.send_message(call.message.chat.id, "tut buted binary tree)")
 
 
 bot.polling()
