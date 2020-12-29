0bd3932fe52e58eeff96930f09fe9f8591a6cb5b
ozahirnyi <ozahirny@gmail.com>
2020-12-29 18:00:02 +0200

    Global tree added, choose helper works but still have seg faults)

--- back/binary.py
+++ back/binary.py
@@ -1,21 +1,22 @@
 from binarytree import Node
 from back.config import bot
+from back.config import ch_keyboard
 
 
 # Binary tree filling
 def binary_init():
-    b_tree = Node(1)
-    b_tree.left = Node(2)
-    b_tree.right = Node(3)
-    b_tree.left.left = Node(3)
-    b_tree.left.left.left = Node(4)
-    b_tree.left.right = Node(3)
-    b_tree.right.left = Node(2)
-    b_tree.right.right = Node(2)
-    b_tree.right.right.right = Node(3)
-    b_tree.right.left.left = Node(5)
-    b_tree.left.right.right = Node(5)
-    return tree
+    b_tree = Node(0)
+    b_tree.left = Node(1)
+    b_tree.right = Node(2)
+    b_tree.left.left = Node(2)
+    b_tree.left.left.left = Node(3)
+    b_tree.left.right = Node(2)
+    b_tree.right.left = Node(1)
+    b_tree.right.right = Node(1)
+    b_tree.right.right.right = Node(2)
+    b_tree.right.left.left = Node(4)
+    b_tree.left.right.right = Node(4)
+    return b_tree
 
 
 # Enum getattr
@@ -32,18 +33,31 @@ tree = binary_init()
 
 @bot.callback_query_handler(func=lambda call: True)
 def tree_helper_init(call):
-    moved_tree = Node(0)
+    global tree
 
     if call.message:
+        if tree is not None:
+            print(tree.value)
+            print(tree)
+        else:
+            bot.send_message(call.message.chat.id, "This is your choice)")
         if call.data == "First":
-            moved_tree = tree.left
+            print("First")
+            tree = tree.left
         elif call.data == "Second":
-            moved_tree = tree.right
-            bot.send_message(call.message.chat.id, "Do you like it?")
-            img = open("resources/" + Colors[tree.value] + ".png", 'rb')
-            bot.send_photo(call.message.chat.id, img)
+            print("Second")
+            tree = tree.right
+        bot.send_message(call.message.chat.id, "Do you like it?")
+        img = open("resources/" + Colors[tree.value] + ".png", 'rb')
+        bot.send_photo(call.message.chat.id, img, reply_markup=ch_keyboard)
 
 
 def main(call):
+
+    print(tree)
     bot.send_message(call.message.chat.id, "Privet Ya Tebe pomogu)")
+    img = open("resources/pink.png", 'rb')
+    bot.send_message(call.message.chat.id, "Do you like it?")
+    bot.send_photo(call.message.chat.id, img, reply_markup=ch_keyboard)
+
 
